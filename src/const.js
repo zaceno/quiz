4dd489aa5ee6781838d06e61c95534f135b653f5
Zacharias Enochsson <zache@axis.com>
2020-01-14 00:16:55 +0100

    add real questions

--- src/const.js
+++ src/const.js
@@ -1,2 +1,4 @@
-export const POOL_SIZE = 20
+import QUESTIONS from '../questions.json'
+export const POOL_SIZE = Object.keys(QUESTIONS).length
 export const SERIES_LENGTH = 10
+export { QUESTIONS }
--- src/lib/fetch-question.js
+++ src/lib/fetch-question.js
@@ -1,15 +1,12 @@
+import { QUESTIONS } from '../const'
+
 /*
     phony question-fetch as a hyperapp effect
 */
 
 const fetchQuestion = (dispatch, { action, id }) =>
     setTimeout(_ => {
-        dispatch(action, {
-            id,
-            question: `Is the question with id ${id}?`,
-            options: ['A', 'B', 'C', 'D'],
-            correct: 2,
-        })
+        dispatch(action, { id, ...QUESTIONS[id] })
     }, 400) //emulate network delay
 
 export default (id, action) => [fetchQuestion, { action, id }]
